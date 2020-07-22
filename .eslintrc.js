f0ffb3862f973553cf3f2d9f83aaded25d896b46
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-07-21 22:09:14 -0400

    Updated project to support react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,57 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2020: true,
+//     jest: true,
+//   },
+//   extends: [
+//     // 'plugin:react/recommended',
+//     'airbnb-typescript',
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 11,
+//     project: "./tsconfig.json",
+//     sourceType: 'module',
+//   },
+//   plugins: [
+//     // 'react',
+//     '@typescript-eslint',
+//   ],
+//   rules: {
+//     'linebreak-style': 0,
+//     'import/extensions': 0,
+//     'max-len': ['error', { code: 120 }],
+//     // 'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+//   },
+//   settings: {
+//     'import/resolver': {
+//       node: {
+//         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+//       },
+//     },
+//   },
+// };
+
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    jest: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+  "extends": [
+    "airbnb-typescript"
   ],
-  rules: {
-    'linebreak-style': 0,
-    'import/extensions': 0,
-    'max-len': ['error', { code: 120 }],
+  "parserOptions": {
+    "project": "./tsconfig.json",
+    "sourceType": "module",
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
+  "rules": {
+    "linebreak-style": "off",
+    "react/prop-types": "off",
+    "max-len": ["error", { "code": 120 }]
+  }
 };
